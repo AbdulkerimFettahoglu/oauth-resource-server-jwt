65060d35f5b4c599480b2c1584ce9c7496dacf2c
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-05-04 19:17:31 +0300

    configuration about RSAKeyProperties class.

--- src/main/java/dev/kerimfettahoglu/oauthresourceserver/OauthResourceServerApplication.java
+++ src/main/java/dev/kerimfettahoglu/oauthresourceserver/OauthResourceServerApplication.java
@@ -1,9 +1,12 @@
 package dev.kerimfettahoglu.oauthresourceserver;
 
+import dev.kerimfettahoglu.oauthresourceserver.configuration.RSAKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RSAKeyProperties.class)
 public class OauthResourceServerApplication {
 
 	public static void main(String[] args) {
